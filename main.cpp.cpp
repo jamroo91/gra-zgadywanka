2203822b67e321807e5ca616858c05dacd151446
Bartosz Boligłowa <boliglowa.bartosz@gmail.com>
2023-04-15 17:30:18 +0200

    zgadywanka

---
+++ main.cpp.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream> 
+#include <cstdlib> 
+#include <ctime> 
+using namespace std; 
+
+int losowanie(int N) { 
+	int w; 
+	srand(time(0)); 
+	for (int i = 0; i < 50; i++)
+		w = rand() % (N + 1); 
+	return w;
+}
+
+
+void zgadywanka() { 
+	int N = 1000; 
+	int liczba = losowanie(N); 
+	int zgadnij; 
+	cout << "zgadnij liczbę z przedziału 0 - " << N << endl;
+	do {
+		cin >> zgadnij;
+		if (zgadnij < liczba) {
+			cout << "Za mało, spróbuj ponownie." << endl;
+		}
+		else if (zgadnij > liczba) {
+			cout << "Za dużo, spróbuj ponownie." << endl;
+		}
+		else {
+			cout << "Brawo! Odgadłeś liczbę." << endl;
+		}
+	} while (zgadnij != liczba);
+}
+
+/
+int main() {
+	setlocale(LC_ALL, "");
+	zgadywanka();
+	
+	return 0;
+}
